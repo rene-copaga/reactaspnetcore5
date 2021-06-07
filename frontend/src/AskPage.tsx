83f4664eb8160175f325eab17aafe2a100d3c310
Rene Copaga <rene.copaga@gmail.com>
2021-06-06 23:35:08 -0400

    -Implementing the ask form

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -1,5 +1,41 @@
 import React from 'react';
 import { Page } from './Page';
+import {
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldInput,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from './Styles';
+import { useForm } from 'react-hook-form';
 
-export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+type FormData = {
+  title: string;
+  content: string;
+};
+
+export const AskPage = () => {
+  const { register } = useForm<FormData>();
+  return (
+    <Page title="Ask a question">
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput id="title" type="text" {...register('title')} />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea id="content" {...register('content')} />
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
+  );
+};
 export default AskPage;
