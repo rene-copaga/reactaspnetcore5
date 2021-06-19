06e306ed933423aeb0f908514ad482c7b1876ca3
Rene Copaga <rene.copaga@gmail.com>
2021-06-18 23:02:55 -0400

    Reading data using Dapper -Creating the repository class

---
+++ backend/QandA/Data/DataRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+
+        public DataRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
