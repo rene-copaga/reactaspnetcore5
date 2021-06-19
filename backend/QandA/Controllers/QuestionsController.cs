7309a9328d9a36b098b74adba6e64a2ecd402d21
Rene Copaga <rene.copaga@gmail.com>
2021-06-19 16:01:46 -0400

    -Injecting the data repository into the API controller

--- backend/QandA/Controllers/QuestionsController.cs
+++ backend/QandA/Controllers/QuestionsController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using QandA.Data;
+using QandA.Data.Models;
 
 namespace QandA.Controllers
 {
@@ -11,5 +13,11 @@ namespace QandA.Controllers
     [ApiController]
     public class QuestionsController : ControllerBase
     {
+        private readonly IDataRepository _dataRepository;
+
+        public QuestionsController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
     }
 }
