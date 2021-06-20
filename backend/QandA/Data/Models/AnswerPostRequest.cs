3ac36ce22d9dab3e4c027e8eecf78383bdf6b2ce
Rene Copaga <rene.copaga@gmail.com>
2021-06-19 23:40:31 -0400

    -Adding validation to posting an answer

--- backend/QandA/Data/Models/AnswerPostRequest.cs
+++ backend/QandA/Data/Models/AnswerPostRequest.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QandA.Data.Models
 {
     public class AnswerPostRequest
     {
-        public int QuestionId { get; set; }
+        [Required]
+        public int? QuestionId { get; set; }
+        [Required]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
