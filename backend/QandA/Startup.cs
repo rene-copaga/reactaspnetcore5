0f3d5fdc8cab8ba123dd1ce7601f1bdb941064ed
Rene Copaga <rene.copaga@gmail.com>
2021-06-21 08:28:27 -0400

    Setting up Auth0 with our ASP.NET backend -Setting up Auth0 -Configuring our ASP.NET backend to authenticate with Auth0

--- backend/QandA/Startup.cs
+++ backend/QandA/Startup.cs
@@ -13,6 +13,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DbUp;
 using QandA.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace QandA
 {
@@ -55,6 +56,16 @@ namespace QandA
 
             services.AddMemoryCache();
             services.AddSingleton<IQuestionCache, QuestionCache>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Authority"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +83,8 @@ namespace QandA
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
