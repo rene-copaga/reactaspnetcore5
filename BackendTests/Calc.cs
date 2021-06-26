328beb74c0b6440b478aa8dadeff9b9e3379cb11
Rene Copaga <rene.copaga@gmail.com>
2021-06-26 08:25:36 -0400

    Implementing .NET tests with xUnit -Getting started with xUnit

---
+++ BackendTests/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackendTests
+{
+    public static class Calc
+    {
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
