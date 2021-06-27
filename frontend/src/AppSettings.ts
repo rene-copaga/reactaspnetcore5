d5e9bc38b12f6e7a185c51b2142638f9fabf90ef
Rene Copaga <rene.copaga@gmail.com>
2021-06-27 14:31:01 -0400

    Configuring the React frontend for staging and production
    npm install cross-env --save-dev

--- frontend/src/AppSettings.ts
+++ frontend/src/AppSettings.ts
@@ -1,4 +1,9 @@
-export const server = 'https://localhost:5001';
+export const server =
+  process.env.REACT_APP_ENV === 'production'
+    ? 'https://qanda2021test-backend.azurewebsites.net'
+    : process.env.REACT_APP_ENV === 'staging'
+    ? 'https://qanda2021staging-backend.azurewebsites.net'
+    : 'https://localhost:5001';
 export const webAPIUrl = `${server}/api`;
 
 export const authSettings = {
